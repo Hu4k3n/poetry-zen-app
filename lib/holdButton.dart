3d1f10b17451311efa9f2627db0a7478a790c4f3
Hu4k3n <arjun.syam23@gmail.com>
2022-05-20 15:19:22 +0530

    added zen audio

--- lib/holdButton.dart
+++ lib/holdButton.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:holding_gesture/holding_gesture.dart';
 import 'package:avatar_glow/avatar_glow.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 class HoldButton extends StatefulWidget {
   HoldButton({Key? key}) : super(key: key);
@@ -12,11 +13,14 @@ class HoldButton extends StatefulWidget {
 
 class _HoldButtonState extends State<HoldButton> with TickerProviderStateMixin {
   late AnimationController _holdButtonController;
+  AssetsAudioPlayer zenAudioPlayer = AssetsAudioPlayer();
   @override
   void initState() {
     super.initState();
     _holdButtonController = AnimationController(
         vsync: this, duration: Duration(seconds: 1, milliseconds: 700));
+    zenAudioPlayer.open(Audio('assets/audio/audio_zen.mp3'),
+        autoStart: false, showNotification: false, loopMode: LoopMode.single);
   }
 
   @override
@@ -37,12 +41,15 @@ class _HoldButtonState extends State<HoldButton> with TickerProviderStateMixin {
           ),
           onTapDown: (_) {
             _holdButtonController.repeat(reverse: false);
+            zenAudioPlayer.play();
           },
           onTapUp: (_) {
             _holdButtonController.reverse();
+            zenAudioPlayer.pause();
           },
           onTapCancel: () {
             _holdButtonController.reverse();
+            zenAudioPlayer.stop();
           },
         )));
   }
